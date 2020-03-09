39e8d89193b392de17ba95717f8248c515f6f578
E2AA09FFB978F2A7DAD07689B8B54CDB04B8286FBB6401EE76DC102C8CB51F1C <48220109+iadgovuser41@users.noreply.github.com>
2020-03-09 10:54:28 -0400

    Create dirComparsion.ps1

---
+++ dirComparsion.ps1
@@ -0,0 +1,63 @@
+<#
+.SYNOPSIS
+Find new or changed files in a directory compared to a known-good image.
+
+.DESCRIPTION
+The script looks for file changes/additions between a production directory (target) with a known-good directory.
+
+.PARAMETER knownGood
+Path of the known-good directory.
+
+.PARAMETER productionImage
+Path of the production directory (target).
+
+.INPUTS
+System.String
+
+.OUTPUTS
+System.String
+
+.EXAMPLE
+.\dirComparsion.ps1 -knownGood <PATH> -productionImage <PATH>
+.\dirComparsion.ps1 -knownGood .\knownGoodDir\ -productionImage .\targetDir\
+.\dirComparsion.ps1 -knownGood "D:\release3.0" -productionImage "C:\inetpub\wwwroot"
+
+-- Input --
+.\dirComparsion.ps1 -knownGood "D:\Users\<user>\Documents\knownGoodDir" -productionImage "C:\Users\<user>\Documents\targetDir"
+
+-- Output --
+File analysis started.
+Any file listed below is a new or changed file.
+
+C:\Users\<user>\Documents\targetDir\index.html
+C:\Users\<user>\Documents\targetDir\research.docx
+C:\Users\<user>\Documents\targetDir\inventory.csv
+C:\Users\<user>\Documents\targetDir\contactus.js
+
+File analysis completed.
+
+.LINK
+https://github.com/nsacyber/MitigatingWebShells
+#>
+
+<#
+#
+# Execution begins.
+#
+#>
+param (
+    [Parameter(Mandatory=$TRUE)][ValidateScript({Test-Path $_ -PathType 'Container'})][String] $knownGood,
+    [Parameter(Mandatory=$TRUE)][ValidateScript({Test-Path $_ -PathType 'Container'})][String] $productionImage
+)
+
+# Recursively get all files in both directories, for each file calculate hash.
+$good = Get-ChildItem -Force -Recurse -Path $knownGood | ForEach-Object { Get-FileHash -Path $_.FullName }
+$prod = Get-ChildItem -Force -Recurse -Path $productionImage | ForEach-Object { Get-FileHash -Path $_.FullName }
+
+Write-Host "File analysis started."
+Write-Host "Any file listed below is a new or changed file.`n"
+
+# Compare files hashes, select new or changed files, and print the path+filename.
+(Compare-Object $good $prod -Property hash -PassThru | Where-Object{$_.SideIndicator -eq '=>'}).Path
+
+Write-Host "`nFile analysis completed."
